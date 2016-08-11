1c28b8f2a84a2277c9accf9bb47b4b605349597a
zhangtiantian10 <975571814@qq.com>
2016-08-11 14:26:28 +0800

    write log in with mongodb

---
+++ mongodb.js
@@ -0,0 +1,68 @@
+const MongoClient = require('mongodb').MongoClient;
+const DB_CONN_STR = 'mongodb://localhost:27017/BabyUser';
+
+const findOne = (req, res) => {
+    const userInfo = {name: req.body.name};
+
+    MongoClient.connect(DB_CONN_STR,(err, db)=> {
+        console.log("link ok");
+        const collection = db.collection('user');
+        collection.find(userInfo).toArray(function (err, docs) {
+            console.log(docs);
+            const user = docs.find(doc => doc.password === req.body.password);
+            console.log(user);
+
+            if(user) {
+                req.session.name = user.name;
+                res.send(true);
+            } else {
+                res.send(false);
+            }
+        });
+        db.close();
+    })
+};
+
+const insertOne = function (req,res) {
+    const userInfo = req.body;
+
+    MongoClient.connect(DB_CONN_STR,(err, db)=> {
+        console.log("link ok");
+        const collection = db.collection('user');
+        const result = collection.insertOne(userInfo);
+        res.json(result);
+        db.close();
+    })
+};
+
+const deleteOne = function (req,res) {
+    const userName = req.body;
+
+    MongoClient.connect(DB_CONN_STR,(err, db)=> {
+        console.log("link ok");
+        const collection = db.collection('user');
+        const result = collection.removeOne(userName);
+        res.json(result);
+        db.close();
+    })
+};
+
+const updateOne = function (req,res) {
+    const userInfo = req.body;
+    const oldInfo = {user:userInfo.user,pwd:userInfo.pwd};
+
+    MongoClient.connect(DB_CONN_STR,(err, db)=> {
+        console.log("link ok");
+        const collection = db.collection('user');
+        const result = collection.updateOne(oldInfo,{$set:{pwd:userInfo.newPwd}});
+        res.json(result);
+        db.close();
+    })
+};
+
+module.exports = {
+    findOne,
+    insertOne,
+    deleteOne,
+    updateOne
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const DB_CONN_STR = 'mongodb://localhost:27017/BabyUser';
+const mdb = require('./mongodb');
 const fs = require('fs');
 const app = new express();
 const session = require('express-session');
@@ -28,10 +29,13 @@ app.get('/logIn', (req, res) => {
     }
 });
 
-app.post('/logIn', (req, res) => {
-    req.session.name = req.body.name;
-    res.send(true)
-});
+app.post('/logIn', mdb.findOne);
+
+// app.post('/logIn', (req, res) => {
+//
+//     req.session.name = req.body.name;
+//     res.send(true)
+// });
 
 app.listen(3000, () => {
     console.log('Server start');
