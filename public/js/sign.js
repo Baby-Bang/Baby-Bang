f0c9dbca680c36fcf9bd4f33726f0d604fed41c6
zhangtiantian10 <975571814@qq.com>
2016-08-12 18:55:05 +0800

    sign in view add style

--- public/js/sign.js
+++ public/js/sign.js
@@ -7,27 +7,36 @@ export default class Sign extends React.Component {
         this.state = {
             isUserExist: false,
             isSamePassword: true,
+            isDisabled: ''
         }
     }
 
     existName() {
         $.post('/existUser', {userName: document.getElementById('userName').value}, (isUserExist) => {
-            this.setState({isUserExist}, () => {
-                if (this.state.isUserExist) {
+            if (isUserExist) {
+                this.setState({isUserExist, isDisabled: 'disabled'}, () => {
                     document.getElementById('userExist').innerHTML = ' 该用户已存在';
-                } else {
-                    document.getElementById('userExist').innerHTML = '';
+                });
+            } else {
+                console.log(this.state.isSamePassword);
+                this.setState({isUserExist});
+                if (this.state.isSamePassword) {
+                    this.setState({isDisabled: ''});
                 }
-            })
-        })
+                document.getElementById('userExist').innerHTML = '';
+            }
+        });
     }
 
     isSamePassword() {
         if (document.getElementById('password').value === document.getElementById('confirmPassword').value) {
+            if(!this.state.isUserExist){
+                this.setState({isDisabled: ''});
+            }
             this.setState({isSamePassword: true});
             document.getElementById('samePassword').innerHTML = '';
         } else {
-            this.setState({isSamePassword: false});
+            this.setState({isSamePassword: false, isDisabled: 'disabled'});
             document.getElementById('samePassword').innerHTML = '两次密码输入不一致';
         }
     }
@@ -37,12 +46,12 @@ export default class Sign extends React.Component {
         const password = document.getElementById('password').value;
         const confirmPassword = document.getElementById('confirmPassword').value;
         const babyBir = document.getElementById('babyBir').value;
-        const sex=$("input[name=parent]:checked").val();
+        const sex = $("input[name=parent]:checked").val();
         if (userName === '' || password === '' ||
             confirmPassword === '' || babyBir === '' || sex === undefined) {
             alert('您所输入的注册内容不完整！');
         } else {
-            $.post('/userInfo',{name:userName,password:password,babyBir:babyBir,sex:sex});
+            $.post('/userInfo', {name: userName, password: password, babyBir: babyBir, sex: sex});
             browserHistory.push('/');
 
         }
@@ -80,18 +89,19 @@ export default class Sign extends React.Component {
                         <input type="date" className="form-control" id="babyBir" placeholder=""/>
                     </div>
                 </div>
-                <br/>
                 <div className="form-group">
                     <div className="col-md-4 col-md-offset-3">
-                        <input type="radio" placeholder="" name="parent" value="woman"/>妈妈
+                        <input  type="radio" placeholder="" name="parent" value="woman"/><span className="sex">妈妈</span>
                     </div>
                     <div className="col-md-4">
-                        <input type="radio" placeholder="" name="parent" value="man"/>爸爸
+                        <input className="sex" type="radio" placeholder="" name="parent" value="man"/><span className="sex">爸爸</span>
                     </div>
                 </div>
                 <div className="form-group">
 
-                    <button type="button" className="btn btn-default" onClick={this.formSubmit.bind(this)}>注册</button>
+                    <button type="button" className="btn btn-default signbtn" disabled={this.state.isDisabled}
+                            onClick={this.formSubmit.bind(this)}>注册
+                    </button>
                 </div>
             </form>
         </div>
