9ff31af9a4b07cda1b2d5b8aedb86b1ee7352d2c
李杨帆 <2531349593@qq.com>
2016-08-13 09:59:15 +0800

    modify growup link

--- public/js/growth-Editor.js
+++ public/js/growth-Editor.js
@@ -54,61 +54,9 @@ class Text extends Component{
 }
 
 class Start extends Component{
-    componentWillMount(){
-        var starGroup = document.getElementById('star').children,
-            clickIndex; //定义一个全局变量用于储存被点击星星的索引值
-
-        window.onload = starFunc;
-
-        function starFunc() {
-            for (var i = 0; i < starGroup.length; i++) {//将每颗星星循环出来
-                starGroup[i].onmouseover = starHover;//绑定鼠标划到星星上的事件
-                starGroup[i].onclick = starClick;//绑定点击星星事件
-                starGroup[i].onmouseout = starOut;//绑定鼠标移出星星事件
-                starGroup[i].index = i;//获得每颗星星的索引
-            };
-        }
-
-        function starHover() {
-            var curIndex = this.index;//鼠标划过星星时，获得当前星星索引值
-            starLight(curIndex);//传入点亮星星的函数，从而执行点亮和熄灭动作
-        }
-
-        function starClick() {
-            var curIndex = this.index;//获得当前被点击星星的索引值
-            starLight(curIndex);//传入点亮星星的函数
-            clickIndex = curIndex;//将被点击星星的索引值记录下来
-        }
-
-        function starOut() {
-//如果点击事件发生，被点击星星索引传入点亮星星函数，鼠标划出时将恢复点击时的状态，如果点击事件没有发生，星星将全部熄灭
-            starLight(clickIndex);
-        }
-
-        function starLight(index) {//点亮星星函数，当mouse over和点击事件发生时，将当前星星索引值传入，用以执行星星的点亮和熄灭动作
-            for (var i = 0; i < starGroup.length; i++) {
-                if (i <= index) {
-                    starGroup[i].className = "rated";//点亮星星
-                } else {
-                    starGroup[i].className = "";//熄灭星星
-                };
-            };
-        }
-
-
-    }
     render(){
-        return <div id="star">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+        return <div>
+
         </div>
     }
 }
